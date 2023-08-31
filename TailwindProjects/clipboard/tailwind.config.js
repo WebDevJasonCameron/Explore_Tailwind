d3ac659ee55a76c4e9918c3b45a2f90c4aa1542c
Jason Cameron <dev.jasoncameron@gmail.com>
2023-08-31 05:50:08 -0500

    set up

--- TailwindProjects/clipboard/tailwind.config.js
+++ TailwindProjects/clipboard/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+      screens: {
+          sm: '480',
+          md: '768',
+          lg: '976',
+          xl: '1440',
+      },
+    extend: {
+        colors: {
+          strongCyan: 'hsl(171, 66%, 44%)',
+          lightBlue: 'hsl(233, 100%, 69%)',
+          darkGrayishBlue: 'hsl(210, 10%, 33%)',
+          graishBlue: 'hsl(201, 11%, 66%)',
+        },
+        fontFamily: {
+            sans: ['Bai Jamjuree', 'sans-serif']
+        }
+    },
   },
   plugins: [],
 }
